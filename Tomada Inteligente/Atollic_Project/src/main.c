78f76b69125bc4d360308351150312a3d57b8273
Lucas Moura <lucassiyahoo.com.br@gmail.com>
2021-12-03 16:00:34 -0300

    feat: commit problem solved

--- Tomada Inteligente/Atollic_Project/src/main.c
+++ Tomada Inteligente/Atollic_Project/src/main.c
@@ -1,18 +1,4 @@
 #include "config.h"
-
-void init(){
-	RCC->AHB1ENR=0x87;
-}
-
-int main(void) {
-	init();
-
-	//BAUD, OVER8, USART_NUN
-	setSerial(9600, 0, 2);
-
-	//PAYLOAD, PAYLOAD_CHAR_LENGTH, TX_DELAY
-	sendSerial("Lucas", sizeof("Lucas") - 1, 10000);
-#include "config.h"
 #include <string.h>
 
 void init(){
